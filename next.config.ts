af18d95565d7d771c43677d95b989237290a48c7
wayanputrawid25-art <wayanputrawid25@gmail.com>
2026-06-06 11:48:07 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["tesseract.js"],
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb"
